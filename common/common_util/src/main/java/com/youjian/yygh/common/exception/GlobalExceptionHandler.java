45a544c59a22f0658611c8ba1453491ac288e25c
hanguanping <youjiannairuo@163.com>
2022-06-13 19:49:14 +0800

    增加 异常统一处理

---
+++ common/common_util/src/main/java/com/youjian/yygh/common/exception/GlobalExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.youjian.yygh.common.exception;
+
+import com.youjian.yygh.common.result.Result;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@ControllerAdvice
+@ResponseBody
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    @ResponseBody
+    public Result error(Exception e) {
+        e.printStackTrace();
+        return Result.fail();
+    }
+
+    @ExceptionHandler(YyghException.class)
+    @ResponseBody
+    public Result error1(Exception e) {
+        e.printStackTrace();
+        return Result.fail();
+    }
+}
